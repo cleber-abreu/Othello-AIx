3437fa24d37adaa329ad4bc1b1506c2808e89149
wiliamtrancoso <wileleh@gmail.com>
2016-12-22 22:40:28 -0200

    move db config to dao package

--- src/main/java/com/tcc/othello/dao/DB.java
+++ src/main/java/com/tcc/othello/dao/DB.java
@@ -1,8 +1,8 @@
-package com.tcc.othello.global;
+package com.tcc.othello.dao;
 
 import java.io.File;
 
-public abstract class DBConfig {
+public abstract class DB {
 	public static final String JDBC = "org.sqlite.JDBC";
 	public static final String JDBC_CONNECTION = "jdbc:sqlite";
 	public static final String DB_FILE_NAME = "res"+File.separator+"database.db";
