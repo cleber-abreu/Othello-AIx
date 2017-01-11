d3a4804b0d93c484002f0538281bc740a1609cf2
Wiliam Trancoso <wtrancoso@qualityautomacao.com>
2017-01-11 09:49:11 -0200

    QP-1837 criacao da baseActivity

---
+++ webPosto/app/src/main/java/com/qualityautomacao/webposto/view/BaseActivity.java
@@ -0,0 +1,62 @@
+package com.qualityautomacao.webposto.view;
+
+import android.app.ProgressDialog;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.qualityautomacao.webposto.R;
+
+/**
+ * @author Wiliam
+ * @since 11/01/2017
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStop() {
+        if(progressDialog != null && progressDialog.isShowing()) progressDialog.dismiss();
+        super.onStop();
+    }
+
+    protected void showLoadDialog(){
+        bloqueiaOrientacao();
+
+        if(progressDialog == null){
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("");
+            progressDialog.setMessage(this.getResources().getString(R.string.load));
+            progressDialog.setIndeterminate(true);
+            progressDialog.setCancelable(false);
+        }
+
+        progressDialog.show();
+    }
+
+    protected void hideLoadDialog(){
+        if(progressDialog != null && progressDialog.isShowing()) progressDialog.dismiss();
+        liberaOrientacao();
+    }
+
+    protected void bloqueiaOrientacao(){
+        switch (this.getResources().getConfiguration().orientation){
+            case Configuration.ORIENTATION_LANDSCAPE:
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                break;
+            default:
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+    }
+
+    protected void liberaOrientacao(){
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+    }
+}
